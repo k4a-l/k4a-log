3a42849a6dcd8ae367e970b3f813fc7941cbdfa8
kasahala <4k.kasahara@gmail.com>
2025-01-27 00:37:15 +0900

    cacheを作らないように
    https://github.com/vercel/next.js/discussions/17218

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	webpack: (config) => {
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+	) => {
+		if (config.cache && !dev) {
+			config.cache = Object.freeze({
+				type: "memory",
+			});
+		}
+
 		config.resolve.fallback = { fs: false };
 
 		return config;
